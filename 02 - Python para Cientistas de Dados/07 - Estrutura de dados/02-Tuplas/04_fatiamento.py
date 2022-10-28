0755317ee290d7038d0152b8719b1494e43da595
Henrique Schilling Klein <58014127+henrique-sk@users.noreply.github.com>
2022-10-28 15:01:08 -0300

    tuplas

---
+++ 02 - Python para Cientistas de Dados/07 - Estrutura de dados/02-Tuplas/04_fatiamento.py
@@ -0,0 +1,8 @@
+lista = ("p", "y", "t", "h", "o", "n",)
+
+print(lista[2:]) # ["t", "h", "o", "n"]
+print(lista[:2]) # ["p", "y"]
+print(lista[1:3]) # ["y", "t"]
+print(lista[0:3:2]) # ["p", "t"] aqui, 3 é o ponto de parada e 2 é o step (de 2 em 2)
+print(lista[::]) # ["p", "y", "t", "h", "o", "n"]
+print(lista[::-1]) # ["n", "o", "h", "t", "y", "p"]
