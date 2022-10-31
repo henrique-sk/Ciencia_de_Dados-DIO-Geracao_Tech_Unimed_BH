8f1147b08d01802d0d02fc9825f62a4cfef166e1
Henrique Schilling Klein <58014127+henrique-sk@users.noreply.github.com>
2022-10-31 20:39:07 -0300

    métodos de classe e métodos estáticos

--- 02 - Python para Cientistas de Dados/13 - Programação Orientada a Objetos/08 - Métodos de classe e métodos estáticos/01_metodos_classe_estatico.py
+++ 02 - Python para Cientistas de Dados/13 - Programação Orientada a Objetos/08 - Métodos de classe e métodos estáticos/01_metodos_classe_estatico.py
@@ -3,18 +3,27 @@ class Pessoa:
         self.nome = nome
         self.idade = idade
 
+    # métodos de classe estão ligados à classe e não ao objeto
+    # visto que recebem um parâmetro que aponta para a classe e não a instância do objeto
     @classmethod
+    # se preciso ter acesso ao contexto da classe utilizo classmethod
     def criar_de_data_nascimento(cls, ano, mes, dia, nome):
+        # cls é uma instância da classe
         idade = 2022 - ano
         return cls(nome, idade)
 
+    # métodos estáticos não recebem um primeiro argumento explícito
+    # é vinculado à classe e não ao objeto
+    # não pode acessar ou modificar o estado da classe
     @staticmethod
+    # não preciso de contexto, nem da classe nem de instância, uso staticmethod
     def e_maior_idade(idade):
         return idade >= 18
 
 
-p = Pessoa.criar_de_data_nascimento(1994, 3, 21, "Guilherme")
+p = Pessoa.criar_de_data_nascimento(1985, 11, 11, "Henrique")
 print(p.nome, p.idade)
 
 print(Pessoa.e_maior_idade(18))
 print(Pessoa.e_maior_idade(8))
+print(Pessoa.e_maior_idade(p.idade))
