5cfb8233f8db7139b3bc7210099a165b7c3e9c95
Henrique Schilling Klein <58014127+henrique-sk@users.noreply.github.com>
2022-10-30 23:53:11 -0300

    encapsulamento

--- 02 - Python para Cientistas de Dados/13 - Programação Orientada a Objetos/05 - Encapsulamento/02_propriedades_exemplo_foo.py
+++ 02 - Python para Cientistas de Dados/13 - Programação Orientada a Objetos/05 - Encapsulamento/02_propriedades_exemplo_foo.py
@@ -2,16 +2,18 @@ class Foo:
     def __init__(self, x=None):
         self._x = x
 
-    @property
+    @property # pega um método e transforma em uma propriedade
     def x(self):
         return self._x or 0
 
-    @x.setter
+    @x.setter # função de propriedade não retorna valor
     def x(self, value):
         self._x += value
 
     @x.deleter
     def x(self):
+        # ao invés de excluir de fato, o que traria um mau comportamento ao código
+        # atribuir 0 deixa ele lá mas com esse valor
         self._x = 0
 
 
--- 02 - Python para Cientistas de Dados/13 - Programação Orientada a Objetos/05 - Encapsulamento/03_propriedades_exemplo_pessoa.py
+++ 02 - Python para Cientistas de Dados/13 - Programação Orientada a Objetos/05 - Encapsulamento/03_propriedades_exemplo_pessoa.py
@@ -3,11 +3,22 @@ class Pessoa:
         self.nome = nome
         self._ano_nascimento = ano_nascimento
 
+    # @property
+    # def nome(self):
+    #     return self._nome
+    # se for só para retornar um atributo, seu uso não faz sentido
+    # somente caso se queira fazer alguma alteração desse atributo
+    #por exemplo:
+    # @nome.setter
+    # def nome(self, value):
+    #     #... lógica para modificar o nome
+    #     pass
+
     @property
     def idade(self):
         _ano_atual = 2022
         return _ano_atual - self._ano_nascimento
 
 
-pessoa = Pessoa("Guilherme", 1994)
+pessoa = Pessoa("Henrique", 1985)
 print(f"Nome: {pessoa.nome} \tIdade: {pessoa.idade}")
