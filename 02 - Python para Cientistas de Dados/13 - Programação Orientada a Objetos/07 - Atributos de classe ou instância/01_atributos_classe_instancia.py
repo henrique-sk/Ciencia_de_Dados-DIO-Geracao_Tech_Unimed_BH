851d8fcd99e750158e5f1042ab187756d3f61c4d
Henrique Schilling Klein <58014127+henrique-sk@users.noreply.github.com>
2022-10-31 19:41:03 -0300

    variáveis de classe e de instância

--- 02 - Python para Cientistas de Dados/13 - Programação Orientada a Objetos/07 - Atributos de classe ou instância/01_atributos_classe_instancia.py
+++ 02 - Python para Cientistas de Dados/13 - Programação Orientada a Objetos/07 - Atributos de classe ou instância/01_atributos_classe_instancia.py
@@ -1,7 +1,9 @@
 class Estudante:
     escola = "DIO"
+    # variáveis de classe são únicas para todos objetos
 
     def __init__(self, nome, matricula):
+        # variáveis de instância são únicas por objeto
         self.nome = nome
         self.matricula = matricula
 
@@ -14,10 +16,11 @@ def mostrar_valores(*objs):
         print(obj)
 
 
-aluno_1 = Estudante("Guilherme", 1)
+aluno_1 = Estudante("Henrique", 1)
 aluno_2 = Estudante("Giovanna", 2)
 mostrar_valores(aluno_1, aluno_2)
 
 Estudante.escola = "Python"
+aluno_1.escola = "Java"
 aluno_3 = Estudante("Chappie", 3)
 mostrar_valores(aluno_1, aluno_2, aluno_3)
