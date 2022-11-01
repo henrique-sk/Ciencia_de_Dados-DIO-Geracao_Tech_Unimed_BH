e22a30134c815e3b528bc96a0100aa132ec378e9
Henrique Schilling Klein <58014127+henrique-sk@users.noreply.github.com>
2022-10-31 21:54:06 -0300

    desafio de código

--- 02 - Python para Cientistas de Dados/testes/teste.py
+++ 02 - Python para Cientistas de Dados/testes/teste.py
@@ -1,12 +1,19 @@
-class Foo:
-    def hello(self):
-        print(self.__class__.__name__.lower())
+salario = int(input())
+mult = 0
 
+if (0 <= salario <= 600):
+    mult = 0.17
+elif (600.1 <= salario <= 900):
+    mult = 0.13
+elif (900.01 <= salario <= 1500):
+    mult = 0.12
+elif (1500.01 <= salario <= 2000):
+    mult = 0.10
+else:
+    mult = 0.05
 
-class Bar(Foo):
-    def hello(self):
-        return super().hello()
+reajuste = salario * mult
 
-
-bar = Bar()
-bar.hello()
+print(f'Novo salario: {(salario + reajuste):.2f}\n' \
+    f'Reajuste ganho: {reajuste:.2f}\n\n' \
+    f'Em percentual: {(mult * 100):.0f} %')
