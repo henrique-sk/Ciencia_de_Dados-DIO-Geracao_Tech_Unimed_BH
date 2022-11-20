974c0f5060c63cba15b36f4b81f9de399066809b
Henrique Schilling Klein <58014127+henrique-sk@users.noreply.github.com>
2022-11-19 23:12:02 -0300

    conhecendo R

---
+++ 05 - Machine Learning/03 - R para Machine Learning/3-primeiro_problema/conhecendo.r
@@ -0,0 +1,17 @@
+print("Hello world!")
+
+objeto1 <- 3*3
+print(objeto1)
+
+objeto2 <- 2*2
+print(objeto2)
+
+objeto3 <- objeto1+objeto2
+print(objeto3)
+
+(casa <- "todas as letras são minúsculas")
+(CASA <- "todas as letras são maiúsculas")
+print(casa)
+print(CASA)
+
+# comentário
---
+++ 05 - Machine Learning/03 - R para Machine Learning/3-primeiro_problema/variáveis_nativas.r
@@ -0,0 +1,6 @@
+print(pi)
+
+print(LETTERS)
+
+print(month.name)
+print(month.abb)
