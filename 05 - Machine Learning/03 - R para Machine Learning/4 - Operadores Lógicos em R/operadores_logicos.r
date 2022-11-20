f0cc762f1c12156b52525a5820cf04c8dac86398
Henrique Schilling Klein <58014127+henrique-sk@users.noreply.github.com>
2022-11-19 23:18:13 -0300

    Create operadores_logicos.r

---
+++ 05 - Machine Learning/03 - R para Machine Learning/4 - Operadores Lógicos em R/operadores_logicos.r
@@ -0,0 +1,18 @@
+#X < Y
+#X > Y
+#X == Y
+#X != Y
+#X => Y
+#X <= Y
+
+X <- 10
+Y <- 20
+print(X > Y)
+
+primeira_letra_primeiro_nome <- 'H'
+primtira_letra_segunddo_nome <- 'S'
+print(primeira_letra_primeiro_nome == primtira_letra_segunddo_nome)
+
+primeiro_nome <- 'Henrique'
+segundo_nome <- 'Schilling'
+print(primeiro_nome != segundo_nome)
