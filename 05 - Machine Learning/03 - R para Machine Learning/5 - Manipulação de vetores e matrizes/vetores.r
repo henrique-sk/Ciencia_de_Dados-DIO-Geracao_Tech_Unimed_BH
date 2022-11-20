9fcc634f2826e6d2d64ab58012abc0606282ca94
Henrique Schilling Klein <58014127+henrique-sk@users.noreply.github.com>
2022-11-19 23:32:49 -0300

    vetores e matrizes

---
+++ 05 - Machine Learning/03 - R para Machine Learning/5 - Manipulação de vetores e matrizes/vetores.r
@@ -0,0 +1,8 @@
+(primeiro.vetor1 <- c(1, 3, 5, 9, 10)) # o "c" é de concatenar
+(primeiro.vetor2 <- c(1, 4, 5, 8, 22))
+
+print(primeiro.vetor1)
+print(length(primeiro.vetor1))
+
+(primeiro.vetor3 <- c(primeiro.vetor1, primeiro.vetor2))
+print(primeiro.vetor3)
